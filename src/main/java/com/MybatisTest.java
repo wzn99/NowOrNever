8d6200ef3c0e3a44652c2bc44302f65660120342
wzn <3533508943@qq.com>
2022-10-05 09:22:00 +0800

    输出2

--- src/main/java/com/MybatisTest.java
+++ src/main/java/com/MybatisTest.java
@@ -25,7 +25,7 @@ public class MybatisTest {
         List<User> users = userMapper.selectAll();
 
         System.out.println(users);
-        System.out.println(users);
+        System.out.println(2);
         sqlSession.close();
     }
 }
